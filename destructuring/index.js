e178346802b229cb0e6ccee5bd5c5adf2f1ff4ba
jonathas <jonathas@localhost.localdomain>
2017-03-14 00:23:26 -0300

    DRAFT2

---
+++ destructuring/index.js
@@ -0,0 +1,6 @@
+const person = { name: 'NewGen', age: 30 }
+ 
+const {name, age} = person
+ 
+console.log(name) // 'NewGen'
+console.log(age) // 30
---
+++ destructuring/indexES5.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var person = { name: 'NewGen', age: 30 };
+
+var name = person.name,
+    age = person.age;
+
+
+console.log(name); // 'NewGen'
+console.log(age); // 30
